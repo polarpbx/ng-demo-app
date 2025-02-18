867f8305bddc5cbdac5abb27c54a4f448aedaf3c
Mario Hvala <hvala.mario@gmail.com>
2025-02-18 22:39:11 +0100

    Created a car list component

--- src/app/features/cars/components/car-list/car-list.component.ts
+++ src/app/features/cars/components/car-list/car-list.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, output } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { CarInterface } from '@models/CarInterface';
+import { AppState } from '@stateTypes/AppState';
+import { carsSelector } from '@store/cars/cars.selectors';
+
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { ButtonGroupModule } from 'primeng/buttongroup';
 
 @Component({
   selector: 'app-car-list',
-  imports: [],
+  imports: [
+    TableModule,
+    ButtonModule,
+    ButtonGroupModule
+  ],
   templateUrl: './car-list.component.html',
   styleUrl: './car-list.component.scss'
 })
 export class CarListComponent {
 
+  private store = inject(Store<AppState>);
+
+  add = output<void>();
+  edit = output<CarInterface>();
+  delete = output<CarInterface>();
+
+  public cars = toSignal<CarInterface[]>(this.store.select(carsSelector));
+  public selectedCar: CarInterface | undefined;
+
+  onAddCar() {
+    this.add.emit();
+  }
+
+  onDetailCar() {
+    this.edit.emit(this.selectedCar!);
+    this.selectedCar = undefined;
+  }
+
+  onDeleteCar() {
+    this.delete.emit(this.selectedCar!);
+    this.selectedCar = undefined;
+  }
 }
